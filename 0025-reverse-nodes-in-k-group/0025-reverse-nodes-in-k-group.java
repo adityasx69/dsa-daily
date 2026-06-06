39c875204b0767fcacbb6baa6ebb4498b5f73fe8
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-06 11:20:22 +0530

    Time: 0 ms (100%), Space: 46.2 MB (87.67%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,28 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null){
-            count++;
+    ListNode reverse(ListNode temp){
+        ListNode prev = null;
+        ListNode curr = temp;
+
+        while(curr!=null){
+            ListNode front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
+
+    ListNode findKthNode(ListNode temp, int k){
+        k--;
+        while(temp != null && k > 0){
             temp = temp.next;
+            k--;
         }
-        temp = head;
-        ListNode x = new ListNode(-1);
-        ListNode newNode = x;
-        for(int i=0;i<Math.floor(count/k);i++){
-            for(int j=0;j<k;j++){
-                stack.push(temp);
-                temp = temp.next;
+        return temp;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp != null){
+            ListNode kthEle = findKthNode(temp,k);
+            if(kthEle == null){
+                if(prev != null){
+                    prev.next = temp;
+                }
+                break;
             }
-            while(!stack.isEmpty()){
-                newNode.next = stack.pop();
-                newNode = newNode.next;
+            ListNode newNode = kthEle.next;
+            kthEle.next = null;
+            reverse(temp);
+            if(temp == head){
+                head = kthEle;
             }
+            else{
+                prev.next = kthEle;
+            }
+            prev = temp;
+            temp = newNode;
         }
-        newNode.next = temp;
-        return x.next;
+        return head;
     }
 }

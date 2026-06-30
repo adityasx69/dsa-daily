c7de1b1ac615d3ea394095f2b9b3a36ef4011627
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-30 11:32:50 +0530

    Time: 14 ms (93.06%), Space: 51.4 MB (74.29%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); Arrays.sort(s);
+        int gp = 0;int sp=0;
+        int count=0;
+        while(gp<g.length && sp<s.length){
+            if(s[sp]>=g[gp]){
+                count++;
+                sp++;
+                gp++;
+            }
+            else{
+                sp++;
+            }
+        }
+        return count;
+    }
+}

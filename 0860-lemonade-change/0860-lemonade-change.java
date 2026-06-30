ba7306a1fac5e2e8c4dd30ea8ec556a1cdc8ecc4
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-30 16:34:45 +0530

    Time: 2 ms (99.35%), Space: 72.5 MB (29.19%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0; int tens=0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                if(five>0){
+                    five--;
+                    tens++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(five>0 && tens>0){
+                    five--;
+                    tens--;
+                }
+                else if(five>2){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

b20646d170d5279c4597cfe174fdb817d6b279f7
Aditya Saraswat <saraswataditya664@gmail.com>
2026-07-03 11:43:03 +0530

    Time: 0 ms (100%), Space: 47.8 MB (71.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double ans = 1.0;
+        long nn = n;
+        int sign = 1;
+        if(nn<0){
+            nn = (-1)*nn;
+            sign = -1;
+        }
+        while(nn>0){
+            if(nn%2==1){
+                ans *= x;
+                nn -=1;
+            }
+            else{
+                x *=x;
+                nn /=2;
+            }
+        }
+        if(sign==-1) ans = (double)(1.0)/(double)(ans);
+        return ans;
     }
 }
